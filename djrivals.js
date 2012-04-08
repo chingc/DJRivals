1346f589469df4b238507da7b6d4c37e5cbd58fd
Ching Chow <smwst10@gmail.com>
2012-04-08 19:01:14 -0400

    Reduced the amount of variables and corrected a class name typo.

--- djrivals.js
+++ djrivals.js
@@ -6,22 +6,20 @@ $(document).ready(function () {
         collapsible: true
     });
     $(".pop").accordion({
-        changestart: function (event, ui) {
+        change: function (event, ui) {
             if (ui.newHeader.next().children("p").text()) {  // activate on expand and only if "<p>Loading...</p>" is found
                 var disc = ui.newHeader.text().replace(/[^a-zA-Z0-9]/g, "").toLowerCase(),
                     chart = ui.newHeader.parent().parent().prev().text().toLowerCase();
                 $.getJSON("./rankings/pop/disc/" + disc + ".json", function (data) {
-                    var dj_table,
-                        rival_table,
-                        dj_records = [],
+                    var dj_records = [],
                         rival_records = [],
                         rival_no_play = [],
                         rival = ["Pakachu", "cgcgngng", "Dan.", "Absurd", "Serika", "DJJEFF20", "Kiba", "Rydia", "evenflow", "Scar", "BluBombr", "Kyon", "Mariposa", "EPHSHI", "novi", "VOXUP", "Butters", "yyr", "Cindy!"];
-                    dj_records.push('<tr><th class="rank">Rank</th><th class="icon">Icon</th><th class="djname">DJ</th><th class="score">Score</th></tr>');
-                    rival_records.push('<tr><th class="rank">Rank</th><th class="icon">Icon</th><th class="djname">Rival DJ</th><th class="score">Score</th></tr>');
+                    dj_records.push('<tr><th class="rank">Rank</th><th class="djicon">Icon</th><th class="djname">DJ</th><th class="score">Score</th></tr>');
+                    rival_records.push('<tr><th class="rank">Rank</th><th class="djicon">Icon</th><th class="djname">Rival DJ</th><th class="score">Score</th></tr>');
                     $.each(data.ranking[chart], function (index, value) {
                         var rank = '<td class="rank">' + value[0] + "</td>",
-                            djicon = '<td class="icon"><img width="52" height="32" src="./images/icon/' + value[1] + '" /></td>',
+                            djicon = '<td class="djicon"><img width="52" height="32" src="./images/icon/' + value[1] + '" /></td>',
                             djname = '<td class="djname">' + value[2] + "</td>",
                             score = '<td class="score">' + value[3] + "</td>",
                             rival_index = $.inArray(value[2], rival);
@@ -32,13 +30,13 @@ $(document).ready(function () {
                         }
                     });
                     $.each(rival, function (index, value) {
-                        rival_no_play.push('<tr><td class="rank">.</td><td class="icon"></td><td class="djname">' + value + '</td><td class="score">0</td></tr>');
+                        rival_no_play.push('<tr><td class="rank">.</td><td class="djicon"></td><td class="djname">' + value + '</td><td class="score">0</td></tr>');
                     });
-                    dj_table = $("<table/>", { html: dj_records.join("\n") });
-                    rival_table = $("<table/>", { html: (rival_records.concat(rival_no_play)).join("\n") });
+                    dj_records = $("<table/>", { html: dj_records.join("") });
+                    rival_records = $("<table/>", { html: (rival_records.concat(rival_no_play)).join("") });
                     ui.newHeader.next().html('<table><tr><td class="djs"></td><td class="rivals"></td></tr></table>');
-                    ui.newHeader.next().find(".djs").html(dj_table);
-                    ui.newHeader.next().find(".rivals").html(rival_table);
+                    ui.newHeader.next().find(".djs").html(dj_records);
+                    ui.newHeader.next().find(".rivals").html(rival_records);
                 });
             }
         }
