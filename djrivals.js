74fbaf084d0424ffee4e14b0adcae116eb261216
Ching Chow <smwst10@gmail.com>
2012-04-11 19:12:22 -0400

    Code clean up.

--- djrivals.js
+++ djrivals.js
@@ -1,27 +1,32 @@
 $(document).ready(function () {
     "use strict";
-    $(".accordion").accordion({
-        active: false,
-        autoHeight: false,
-        collapsible: true
-    });
-    $(".pop").accordion({
-        change: function (event, ui) {
+    var me = "cgcgngng",
+        rivals = ["Pakachu", "cgcgngng", "Dan.", "Absurd", "Serika", "DJJEFF20", "Kiba", "Rydia", "evenflow", "Scar", "BluBombr", "Kyon", "Mariposa", "EPHSHI", "novi", "VOXUP", "Butters", "yyr", "Cindy!"],
+        default_accordion = {
+            active: false,
+            animated: false,
+            autoHeight: false,
+            collapsible: true
+        },
+        pop_accordion_function = function (event, ui) {
             if (ui.newHeader.next().children("p").text()) {  // activate on expand and only if "<p>Loading...</p>" is found
                 var disc = ui.newHeader.text().replace(/[^a-zA-Z0-9]/g, "").toLowerCase(),
                     chart = ui.newHeader.parent().parent().prev().text().toLowerCase();
-                $.getJSON("./rankings/pop/disc/" + disc + ".json", function (data) {
+                $.ajax({
+                    url: "./rankings/pop/disc/" + disc + ".json",
+                    dataType: "json"
+                }).done(function (data) {
                     var dj_records = [],
                         rival_records = [],
                         rival_no_play = [],
-                        rival = ["Pakachu", "cgcgngng", "Dan.", "Absurd", "Serika", "DJJEFF20", "Kiba", "Rydia", "evenflow", "Scar", "BluBombr", "Kyon", "Mariposa", "EPHSHI", "novi", "VOXUP", "Butters", "yyr", "Cindy!"];
-                    dj_records.push('<tr><th class="rank">Rank</th><th class="djicon">Icon</th><th class="djname">DJ</th><th class="score">Score</th></tr>');
-                    rival_records.push('<tr><th class="rank">Rank</th><th class="djicon">Icon</th><th class="djname">Rival DJ</th><th class="score">Score</th></tr>');
+                        rival = rivals.slice(0);  // make local copy for modification
+                    dj_records.push("<tr><th>Rank</th><th>Icon</th><th>DJ</th><th>Score</th></tr>");
+                    rival_records.push("<tr><th>Rank</th><th>Icon</th><th>Rival DJ</th><th>Score</th></tr>");
                     $.each(data.ranking[chart], function (index, value) {
-                        var rank = '<td class="rank">' + value[0] + "</td>",
-                            djicon = '<td class="djicon"><img width="52" height="32" src="./images/icon/' + value[1] + '" /></td>',
-                            djname = '<td class="djname">' + value[2] + "</td>",
-                            score = '<td class="score">' + value[3] + "</td>",
+                        var rank = "<td>" + value[0] + "</td>",
+                            djicon = '<td><img src="./images/icon/' + value[1] + '" /></td>',
+                            djname = "<td>" + value[2] + "</td>",
+                            score = "<td>" + value[3] + "</td>",
                             rival_index = $.inArray(value[2], rival);
                         dj_records.push("<tr>" + rank + djicon + djname + score + "</tr>");
                         if (rival_index > -1) {
@@ -30,15 +35,26 @@ $(document).ready(function () {
                         }
                     });
                     $.each(rival, function (index, value) {
-                        rival_no_play.push('<tr><td class="rank">.</td><td class="djicon"></td><td class="djname">' + value + '</td><td class="score">0</td></tr>');
+                        rival_no_play.push("<tr><td>.</td><td></td><td>" + value + "</td><td>0</td></tr>");
                     });
-                    dj_records = $("<table/>", { html: dj_records.join("") });
-                    rival_records = $("<table/>", { html: (rival_records.concat(rival_no_play)).join("") });
+                    dj_records = $("<table>", { html: dj_records.join("") });
+                    rival_records = $("<table>", { html: (rival_records.concat(rival_no_play)).join("") });
                     ui.newHeader.next().html('<table><tr><td class="djs"></td><td class="rivals"></td></tr></table>');
                     ui.newHeader.next().find(".djs").html(dj_records);
                     ui.newHeader.next().find(".rivals").html(rival_records);
                 });
             }
-        }
-    });
+        },
+        button_function = function () {
+            $.ajax({
+                url: "./rankings/pop/dj/" + me + ".html",
+                dataType: "html"
+            }).done(function (data) {
+                $(".me").html(data);
+                $(".me").find(".accordion").accordion(default_accordion);
+            });
+        };
+    $(".accordion").accordion(default_accordion);
+    $(".pop").bind("accordionchange", pop_accordion_function);
+    $("button").click(button_function);
 });
