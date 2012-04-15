ee4916a10658d40d68c0e2d50840f6164090f2df
Ching Chow <smwst10@gmail.com>
2012-04-15 19:11:18 -0400

    DJRivals database updater.

---
+++ update.py
@@ -0,0 +1,40 @@
+"""DJRivals database updater."""
+from random import shuffle
+from time import localtime, sleep, strftime, time
+
+import pop
+import dj
+import image
+import html
+
+
+def continuous():
+    """update() -> None
+
+    Continuous incremental updates of the DJRivals database.
+
+    """
+    while(True):
+        print("Beginning new cycle...\n")
+        disc_list = list(pop.index().keys())
+        interval = int(24 * 60 * 60 / len(disc_list))
+        shuffle(disc_list)
+        for disc in disc_list:
+            pop.database([disc])
+            print("\nNext incremental update at: " + strftime("%H:%M:%S", localtime(time() + interval)))
+            print("Ctrl-C to quit.\n")
+            sleep(interval)
+        dj.database()
+        image.icons()
+        html.html()
+        print("Full database update complete.\n")
+
+
+def index():
+    """index() -> None
+
+    Update the index file and retrieve new disc images if available.
+
+    """
+    pop.index(True)
+    image.discs()
