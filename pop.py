c7dd0a0ade0ae29595e616406e802412bda9e801
Ching Chow <smwst10@gmail.com>
2012-04-15 06:13:26 -0400

    Modified to conform with the updated data structures.

--- pop.py
+++ pop.py
@@ -8,19 +8,19 @@ from common import _clean, _dir_listing, _file_exists, _link, _make_dir
 
 
 def _f_identifier():
-    """Retrieves a disc identifier from the DJMAX site by clean disc name."""
+    """Retrieves a disc identifier from the DJMAX site by disc name."""
     def identifier(disc):
         # DJMAX returns up to 20 identifiers at a time (each page lists 20 discs
-        # except the last page which has less than 20), the cache dictionary
-        # saves these identifers to expedite future lookups.  however, since it
-        # is unknown how long these identifiers stay valid, the cache size has
-        # been limited to about 25% of the total number of available discs.
-        # when the cache exceeds this limit, it will clear itself and rebuild.
+        # except the last page which has less than 20), so this function uses a
+        # cache to expedite future lookups.  however, since it is unknown how
+        # long these identifiers stay valid, the cache size has been limited to
+        # about 25% of the total number of available discs.  when the cache
+        # exceeds this limit, it will clear itself and rebuild.
         if disc not in cache:
             if len(cache) > 20:  # value of 20 means at most 40
                 cache.clear()
             data = json.loads(urlopen(url.format(info[disc]["page"])).read().decode())["DATA"]["RECORD"]
-            cache.update({_clean(record["DISCNAME"]): record["DISCID"] for record in data})
+            cache.update({record["DISCNAME"]: record["DISCID"] for record in data})
         return cache[disc]
 
     url = _link("pop_ranking_page_url")
@@ -71,10 +71,9 @@ def index(refresh=False):
         for page in range(1, 9):
             data = json.loads(urlopen(url.format(page)).read().decode())["DATA"]["RECORD"]
             for record in data:
-                disc = _clean(record["DISCNAME"])
+                disc = record["DISCNAME"]
                 if disc not in index:
-                    index[disc] = OrderedDict(zip(["full", "page", "nm", "hd", "mx", "ex"], ["", "", 0, 0, 0, 0]))
-                index[disc]["full"] = record["DISCNAME"]
+                    index[disc] = OrderedDict(zip(["page", "nm", "hd", "mx", "ex"], [0, 0, 0, 0, 0]))
                 index[disc]["page"] = page
         with open(index_file, "wb") as f:
             f.write(json.dumps(OrderedDict(sorted(index.items())), indent=4).encode())
@@ -103,29 +102,28 @@ def database(disc_list=[]):
     while len(disc_list):
         print("{} discs remaining.".format(len(disc_list)))
         disc = disc_list.pop()
+        clean_disc = _clean(disc)
         charts = ["nm", "hd", "mx"]
         output = OrderedDict()
         output["timestamp"] = int(time.time())
-        output["name"] = OrderedDict(zip(["clean", "full"], [disc, info[disc]["full"]]))
-        output["image"] = OrderedDict([(i, disc + j) for i, j in zip(["eyecatch"] + charts, [".png", "_1.png", "_2.png", "_3.png", "_4.png"])])
-        output["difficulty"] = OrderedDict([(chart, info[disc][chart]) for chart in charts])
-        output["ranking"] = OrderedDict()
-        output["ranking"]["records"] = OrderedDict()
+        output["name"] = disc
+        output["eyecatch"] = clean_disc + ".png"
         for chart in charts:
+            output[chart] = OrderedDict()
+            output[chart]["icon"] = "{}_{}.png".format(clean_disc, 1 if chart == "nm" else 2 if chart == "hd" else 3 if chart == "mx" else 4)
+            output[chart]["difficulty"] = info[disc][chart]
             try:
                 results = _ranking(identifier(disc), chart)
-                output["ranking"][chart] = results
-                output["ranking"]["records"][chart] = len(results)
-                print("{} {} complete.  Sleeping...".format(disc, chart))
+                output[chart]["records"] = len(results)
+                output[chart]["ranking"] = results
+                print("{} {} complete.  Sleeping...".format(clean_disc, chart))
                 time.sleep(10)
             except:
-                print("{} {} error.  Sleeping for 5 minutes before retrying.".format(disc, chart))
+                print("{} {} error.  Sleeping for 5 minutes before retrying.".format(clean_disc, chart))
                 charts.insert(0, chart)
                 time.sleep(300)
-        #output = json.dumps(output, indent=4)
-        #output = re.sub(r'\[\n +(\d+,) \n +(".*",) \n +(".*",) \n +(\d{6})\n +\](,?) ?', r"[\1 \2 \3 \4]\5", output)
-        with open("{}{}.json".format(pop_db_dir, disc), "wb") as f:
+        with open("{}{}.json".format(pop_db_dir, clean_disc), "wb") as f:
             f.write(json.dumps(output).encode())
-        print('Wrote: "{}{}.json"\n'.format(pop_db_dir, disc))
+        print('Wrote: "{}{}.json"\n'.format(pop_db_dir, clean_disc))
     elapsed_time = round((time.time() - start_time) / 60)
     print("Database creation took {} minutes.".format(elapsed_time))
