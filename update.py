1a271575d92a6d7df1bc7dedf346b29a778f2261
Ching Chow <smwst10@gmail.com>
2012-04-15 20:36:13 -0400

    Sort the disc list by timestamp.

--- update.py
+++ update.py
@@ -1,7 +1,9 @@
 """DJRivals database updater."""
-from random import shuffle
+from collections import OrderedDict
 from time import localtime, sleep, strftime, time
+import json
 
+from common import _link
 import pop
 import dj
 import image
@@ -14,13 +16,19 @@ def continuous():
     Continuous incremental updates of the DJRivals database.
 
     """
+    index_file = _link("index_file")
     while(True):
         print("Beginning new cycle...\n")
-        disc_list = list(pop.index().keys())
+        disc_list = pop.index()
+        disc_list = sorted(disc_list.keys(), key=lambda x: disc_list[x]["timestamp"])
         interval = int(24 * 60 * 60 / len(disc_list))
-        shuffle(disc_list)
         for disc in disc_list:
             pop.database([disc])
+            with open(index_file, "rb") as f:
+                data = json.loads(f.read().decode(), object_pairs_hook=OrderedDict)
+            data[disc]["timestamp"] = int(time())
+            with open(index_file, "wb") as f:
+                f.write(json.dumps(data, indent=4).encode())
             print("\nNext incremental update at: " + strftime("%H:%M:%S", localtime(time() + interval)))
             print("Ctrl-C to quit.\n")
             sleep(interval)
