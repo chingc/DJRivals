ba6e1c41b5426b3e8644f3b623ea6045aa3c4413
Ching Chow <smwst10@gmail.com>
2012-04-25 23:56:04 -0400

    Automatically create the other databases on keyboard interrupt.

--- update.py
+++ update.py
@@ -1,46 +1,42 @@
 """DJRivals database updater."""
 from time import localtime, sleep, strftime, time
 
-import pop
 import dj
-import image
 import html
+import image
+import master
+import pop
 
 
-def popdb():
-    """popdb() -> None
+def database():
+    """database() -> None
 
-    Continuous incremental updates of the pop database.
-
-    """
-    while(True):
-        print("Beginning new cycle...\n")
-        disc_list = pop.index()
-        disc_list = sorted(disc_list.keys(), key=lambda x: disc_list[x]["timestamp"])
-        interval = int(24 * 60 * 60 / len(disc_list))
-        for disc in disc_list:
-            pop.database([disc])
-            print("\nNext incremental update at: " + strftime("%H:%M:%S", localtime(time() + interval)))
-            print("Ctrl-C to quit.\n")
-            sleep(interval)
-        print("\nFull database update complete.\n")
-
-
-def djs():
-    """djs() -> None
-
-    Update the DJ database and retrieve any necessary icons.
+    Continuous incremental updates of the database.
 
     """
-    dj.database()
-    image.icons()
+    try:
+        while(True):
+            disc_list = pop.index()
+            disc_list = sorted(disc_list.keys(), key=lambda x: disc_list[x]["timestamp"])
+            interval = int(24 * 60 * 60 / len(disc_list))
+            for disc in disc_list:
+                pop.database([disc])
+                print("\nNext incremental update at: " + strftime("%H:%M:%S", localtime(time() + interval)))
+                print("Ctrl-C to quit.\n")
+                sleep(interval)
+    except KeyboardInterrupt:
+        print("Please wait...")
+        dj.database()
+        master.database()
+        image.icons()
+        print("Done.")
 
 
 def index():
     """index() -> None
 
     Update the index file, generate the html for DJRivals, and retrieve new disc
-    images if available.
+    images if any.
 
     """
     pop.index(True)
