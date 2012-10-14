61f60c960733f85626e9cdc6d6ec1f7dd8432668
Ching Chow <smwst10@gmail.com>
2012-10-14 16:43:24 -0400

    Rewrote dj().

--- database.py
+++ database.py
@@ -18,34 +18,36 @@ def build(mode, name):
     """
     if mode == _.STAR:
         path  = _.STAR_INDEX
-        level = [("nm", _make_dir(_.STAR_DB_DIR))]
+        level = [("nm", _.STAR_DB_DIR)]
     elif mode == _.POP:
         path  = _.POP_INDEX
-        level = [("nm", _make_dir(_.POP_NM_DB_DIR)),
-                 ("hd", _make_dir(_.POP_HD_DB_DIR)),
-                 ("mx", _make_dir(_.POP_MX_DB_DIR)),
-                 ("ex", _make_dir(_.POP_EX_DB_DIR))]
+        level = [
+            ("nm", _.POP_NM_DB_DIR),
+            ("hd", _.POP_HD_DB_DIR),
+            ("mx", _.POP_MX_DB_DIR),
+            ("ex", _.POP_EX_DB_DIR)
+        ]
     elif mode == _.CLUB:
         path  = _.CLUB_INDEX
-        level = [("nm", _make_dir(_.CLUB_DB_DIR))]
+        level = [("nm", _.CLUB_DB_DIR)]
     elif mode == _.MISSION:
         path  = _.MISSION_INDEX
-        level = [("nm", _make_dir(_.MISSION_DB_DIR))]
+        level = [("nm", _.MISSION_DB_DIR)]
     else:
         raise ValueError("invalid game mode")
-    for a, b in level:
-        results = ranking(mode, name, _.CHART[a])
+    for mode_str, db_dir in level:
+        results = ranking(mode, name, _.CHART[mode_str])
         if len(results) == 0:
             continue
         clean_name = _clean(name)
         data = dict()
         data["name"] = name
         data["eyecatch"] = "{}.png".format(clean_name)
-        data["icon"] = "{}_{}.png".format(clean_name, _.CHART[a])
+        data["icon"] = "{}_{}.png".format(clean_name, _.CHART[mode_str])
         data["ranking"] = results
-        with open("{}{}.json".format(b, clean_name), "wb") as f:
+        with open("{}{}.json".format(db_dir, clean_name), "wb") as f:
             f.write(json.dumps(data, indent=2).encode())
-        print('Wrote: "{}{}.json"'.format(b, clean_name))
+        print('Wrote: "{}{}.json"'.format(db_dir, clean_name))
     with open(path, "rb") as f:
         data = json.loads(f.read().decode(), object_pairs_hook=dict)
         data[name]["timestamp"] = int(time.time())
@@ -56,151 +58,74 @@ def build(mode, name):
 def dj():
     """Build a DJ database using information from the local database.
 
-    The database is implemented as a collection of JSON files.  One JSON file is
-    created for each DJ.
-
     """
-    # todo: code cleanup
-    def _dj_struct(name, icon, star, pop, club, mission):
-        dj = OrderedDict()
-        dj["name"] = name
-        dj["icon"] = icon
-        dj["star"] = OrderedDict()
-        dj["star"]["scores"] = OrderedDict(star)
-        dj["star"]["master"] = []
-        dj["pop"] = OrderedDict()
-        dj["pop"]["nm"] = OrderedDict(pop["nm"])
-        dj["pop"]["hd"] = OrderedDict(pop["hd"])
-        dj["pop"]["mx"] = OrderedDict(pop["mx"])
-        dj["pop"]["ex"] = OrderedDict(pop["ex"])
-        dj["pop"]["master"] = []
-        dj["club"] = OrderedDict()
-        dj["club"]["scores"] = OrderedDict(club)
-        dj["club"]["master"] = []
-        dj["mission"] = OrderedDict()
-        dj["mission"]["scores"] = OrderedDict(mission)
-        dj["mission"]["master"] = []
-        return dj
+    def _extract(dj_set, directory):
+        """Extract DJ and name data."""
+        names = dict()
+        for json_file in _list_dir(directory):
+            with open(directory + json_file, "rb") as f:
+                data = json.loads(f.read().decode())
+            dj_set.update([(record[1], record[2]) for record in data["ranking"]])
+            names[data["name"]] = [9999, 0]
+        return names
 
-    dj_db_dir      = _make_dir(_.DJ_DB_DIR)
-    star_db_dir    = _make_dir(_.STAR_DB_DIR)
-    pop_db_dir     = _make_dir(_.POP_DB_DIR)
-    club_db_dir    = _make_dir(_.CLUB_DB_DIR)
-    mission_db_dir = _make_dir(_.MISSION_DB_DIR)
+    def _fill(dj_dict, directory, mode):
+        """Fill in scores for each DJ."""
+        for json_file in _list_dir(directory):
+            with open(directory + json_file, "rb") as f:
+                data = json.loads(f.read().decode())
+            for record in data["ranking"]:
+                dj_dict[record[2]][mode]["scores"][data["name"]] = [record[0], record[3]]
 
-    star_dir_list    = _list_dir(star_db_dir)
-    pop_dir_list     = _list_dir(pop_db_dir)
-    club_dir_list    = _list_dir(club_db_dir)
-    mission_dir_list = _list_dir(mission_db_dir)
+    djs = set()
 
-    djs     = set()
-    star    = OrderedDict()
-    pop     = {"nm": OrderedDict(), "hd": OrderedDict(), "mx": OrderedDict(), "ex": OrderedDict()}
-    club    = OrderedDict()
-    mission = OrderedDict()
+    # extract disc, disc set, and mission names
+    extracted = dict([
+        ("star", _extract(djs, _.STAR_DB_DIR)),
+        ("pop_nm", _extract(djs, _.POP_NM_DB_DIR)),
+        ("pop_hd", _extract(djs, _.POP_HD_DB_DIR)),
+        ("pop_mx", _extract(djs, _.POP_MX_DB_DIR)),
+        ("pop_ex", _extract(djs, _.POP_EX_DB_DIR)),
+        ("club", _extract(djs, _.CLUB_DB_DIR)),
+        ("mission", _extract(djs, _.MISSION_DB_DIR))
+    ])
 
-    star_master    = []
-    pop_master     = []
-    club_master    = []
-    mission_master = []
+    # convert the DJ set into a dictionary
+    djs = {dj[1]: dict([("name", dj[1]), ("icon", dj[0])]) for dj in djs}
 
-    print("Writing DJ files...")
-
-    # gather a list of all DJ names and all rankings that have at least one record
-    for json_file in star_dir_list:
-        with open(star_db_dir + json_file, "rb") as f:
-            data = json.loads(f.read().decode())
-        djs = djs.union([(record[1], record[2]) for record in data["ranking"]])
-        if data["records"]:
-            star[data["name"]] = [9999, 0]
-    for json_file in pop_dir_list:
-        with open(pop_db_dir + json_file, "rb") as f:
-            data = json.loads(f.read().decode())
-        for chart in _.CHARTS:
-            djs = djs.union([(record[1], record[2]) for record in data[chart]["ranking"]])
-            if data[chart]["records"]:
-                pop[chart][data["name"]] = [9999, 0]
-    for json_file in club_dir_list:
-        with open(club_db_dir + json_file, "rb") as f:
-            data = json.loads(f.read().decode())
-        djs = djs.union([(record[1], record[2]) for record in data["ranking"]])
-        if data["records"]:
-            club[data["name"]] = [9999, 0]
-    for json_file in mission_dir_list:
-        with open(mission_db_dir + json_file, "rb") as f:
-            data = json.loads(f.read().decode())
-        djs = djs.union([(record[1], record[2]) for record in data["ranking"]])
-        if data["records"]:
-            mission[data["name"]] = [9999, 0]
-
-    # convert the DJ names set into a dictionary
-    djs = {dj[1]: _dj_struct(dj[1], dj[0], star, pop, club, mission) for dj in djs}
+    # insert extracted into the DJ set
+    for mode in extracted:
+        for dj in djs:
+            djs[dj][mode] = dict()
+            djs[dj][mode]["scores"] = dict(extracted[mode])
+            djs[dj][mode]["master"] = [9999, 0]
 
     # fill in scores
-    for json_file in star_dir_list:
-        with open(star_db_dir + json_file, "rb") as f:
-            data = json.loads(f.read().decode())
-        for record in data["ranking"]:
-            djs[record[2]]["star"]["scores"][data["name"]] = [record[0], record[3]]
-    for json_file in pop_dir_list:
-        with open(pop_db_dir + json_file, "rb") as f:
-            data = json.loads(f.read().decode())
-        for chart in _.CHARTS:
-            for record in data[chart]["ranking"]:
-                djs[record[2]]["pop"][chart][data["name"]] = [record[0], record[3]]
-    for json_file in club_dir_list:
-        with open(club_db_dir + json_file, "rb") as f:
-            data = json.loads(f.read().decode())
-        for record in data["ranking"]:
-            djs[record[2]]["club"]["scores"][data["name"]] = [record[0], record[3]]
-    for json_file in mission_dir_list:
-        with open(mission_db_dir + json_file, "rb") as f:
-            data = json.loads(f.read().decode())
-        for record in data["ranking"]:
-            djs[record[2]]["mission"]["scores"][data["name"]] = [record[0], record[3]]
+    _fill(djs, _.STAR_DB_DIR, "star")
+    _fill(djs, _.POP_NM_DB_DIR, "pop_nm")
+    _fill(djs, _.POP_HD_DB_DIR, "pop_hd")
+    _fill(djs, _.POP_MX_DB_DIR, "pop_mx")
+    _fill(djs, _.POP_EX_DB_DIR, "pop_ex")
+    _fill(djs, _.CLUB_DB_DIR, "club")
+    _fill(djs, _.MISSION_DB_DIR, "mission")
 
-    # convert dictionaries to lists and obtain master scores
-    for dj in djs:
-        star_total    = 0
-        pop_total     = 0
-        club_total    = 0
-        mission_total = 0
-        djs[dj]["star"]["scores"] = sorted([(k, v[0], v[1]) for k, v in djs[dj]["star"]["scores"].items()])
-        star_total = sum([data[2] for data in djs[dj]["star"]["scores"]])
-        for chart in _.CHARTS:
-            djs[dj]["pop"][chart] = sorted([(k, v[0], v[1]) for k, v in djs[dj]["pop"][chart].items()])
-            pop_total += sum([data[2] for data in djs[dj]["pop"][chart]])
-        djs[dj]["club"]["scores"] = sorted([(k, v[0], v[1]) for k, v in djs[dj]["club"]["scores"].items()])
-        club_total = sum([data[2] for data in djs[dj]["club"]["scores"]])
-        djs[dj]["mission"]["scores"] = sorted([(k, v[0], v[1]) for k, v in djs[dj]["mission"]["scores"].items()])
-        mission_total = sum([data[2] for data in djs[dj]["mission"]["scores"]])
-        star_master.append([dj, star_total])
-        pop_master.append([dj, pop_total])
-        club_master.append([dj, club_total])
-        mission_master.append([dj, mission_total])
-    star_master.sort(key=lambda x: x[1], reverse=True)
-    pop_master.sort(key=lambda x: x[1], reverse=True)
-    club_master.sort(key=lambda x: x[1], reverse=True)
-    mission_master.sort(key=lambda x: x[1], reverse=True)
+    # convert score dictionaries to lists
+    for mode in extracted:
+        for dj in djs:
+            djs[dj][mode]["scores"] = [(name, score[0], score[1]) for name, score in djs[dj][mode]["scores"].items()]
 
-    # assign master scores
-    for i, v in enumerate(star_master):
-        djs[v[0]]["star"]["master"] = [i + 1, v[1]]
-    for i, v in enumerate(pop_master):
-        djs[v[0]]["pop"]["master"] = [i + 1, v[1]]
-    for i, v in enumerate(club_master):
-        djs[v[0]]["club"]["master"] = [i + 1, v[1]]
-    for i, v in enumerate(mission_master):
-        djs[v[0]]["mission"]["master"] = [i + 1, v[1]]
+    # calculate and fill master scores
+    for mode in extracted:
+        extracted[mode] = sorted(filter(lambda x: x[1] > 0, [(dj, sum([score[2] for score in djs[dj][mode]["scores"]])) for dj in djs]), key=lambda x: x[1], reverse=True)
+        for rank, score in enumerate(extracted[mode]):
+            djs[score[0]][mode]["master"] = [rank + 1, score[1]]
 
-    # write the DJ files
+    # write DJ files and index
     for k, v in djs.items():
-        with open("{}{}.json".format(dj_db_dir, zlib.crc32(k.encode())), "wb") as f:
-            f.write(json.dumps(v, indent=1).encode())
-
-    # write the DJ index
+        with open("{}{}.json".format(_.DJ_DB_DIR, zlib.crc32(k.encode())), "wb") as f:
+            f.write(json.dumps(v, indent=2).encode())
     with open(_.DJ_INDEX, "wb") as f:
-        f.write(json.dumps([{"id": zlib.crc32(dj.encode()), "name": dj} for dj in sorted(djs.keys())], indent=1).encode())
+        f.write(json.dumps([{"id": zlib.crc32(dj.encode()), "name": dj} for dj in sorted(djs.keys())], indent=2).encode())
 
 
 def master():
@@ -223,3 +148,13 @@ def master():
         results[mode].sort()
         with open(master_db_dir + mode + ".json", "wb") as f:
             f.write(json.dumps({"ranking": results[mode]}, indent=1).encode())
+
+
+_make_dir(_.DJ_DB_DIR)
+_make_dir(_.STAR_DB_DIR)
+_make_dir(_.POP_NM_DB_DIR)
+_make_dir(_.POP_HD_DB_DIR)
+_make_dir(_.POP_MX_DB_DIR)
+_make_dir(_.POP_EX_DB_DIR)
+_make_dir(_.CLUB_DB_DIR)
+_make_dir(_.MISSION_DB_DIR)
