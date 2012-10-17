57cbaf7796a632eb567afabc713e5ea4092cfb5f
Ching Chow <smwst10@gmail.com>
2012-10-17 03:46:53 -0400

    Mostly rewritten.  One instance can now update all databases.

--- update.py
+++ update.py
@@ -1,58 +1,67 @@
 """DJRivals database updater."""
-from time import localtime, sleep, strftime, time
+import threading
+import time
 
 from common import _
 import database
+import html
 import image
 import index
 
 
-def indexes():
-    """Update indexes, retrieve new images if any, and generate the html."""
-    index.touch(_.STAR, True)
-    index.touch(_.POP, True)
-    index.touch(_.CLUB, True)
-    index.touch(_.MISSION, True)
-    image.mode(_.STAR)
-    image.mode(_.POP)
-    image.mode(_.CLUB)
-    image.mode(_.MISSION)
-    index.html()
-
-
-def mode(mode):
-    """Continuous incremental updates of the specified database."""
+def _update(name, mode, stop):
+    """Continuously update the specified database."""
     if mode == _.STAR:
-        data  = index.touch(_.STAR)
         hours = 36
     elif mode == _.POP:
-        data  = index.touch(_.POP)
         hours = 20
     elif mode == _.CLUB:
-        data  = index.touch(_.CLUB)
         hours = 12
     elif mode == _.MISSION:
-        data  = index.touch(_.MISSION)
         hours = 12
     else:
-        raise ValueError("invalid argument")
+        raise ValueError("invalid game mode")
+    while not stop.is_set():
+        data = index.index(mode)
+        wait = (hours * 60 * 60 / len(data))
+        next = sorted(data.keys(), key=lambda x: data[x]["timestamp"], reverse=True).pop()
+        database.build(mode, next)
+        stop.wait(wait)
+    print(name, "thread stopped.")
+
+
+def update():
+    """Continuously update the databases."""
+    stop = threading.Event()
+    for mode_str, mode in (("Star", _.STAR), ("Pop", _.POP), ("Club", _.CLUB), ("Mission", _.MISSION)):
+        threading.Thread(target=_update, args=(mode_str, mode, stop)).start()
     try:
-        while(True):
-            names = sorted(data.keys(), key=lambda x: data[x]["timestamp"])
-            interval = (hours * 60 * 60 / len(names))
-            while names:
-                database.build(mode, names.pop(0))
-                print("\n{} items remaining in this update cycle.".format(len(names)))
-                print("Next incremental update at: {} (Ctrl-C to Quit)\n".format(strftime("%H:%M:%S", localtime(time() + interval))))
-                sleep(interval)
+        while True:
+            time.sleep(10000)
     except KeyboardInterrupt:
+        stop.set()
+        print("Finishing current jobs.  Please wait...")
+        while threading.active_count() > 1:
+            time.sleep(2)
         print("Done.")
 
 
-def finish():
-    """Build non-game mode databases."""
-    database.dj()
-    database.master()
+def indexes():
+    """Update indexes."""
+    for mode in (_.STAR, _.POP, _.CLUB, _.MISSION):
+        index.index(mode, True)
+
+
+def images():
+    """Retrieve new images, if any."""
+    for mode in (_.STAR, _.POP, _.CLUB, _.MISSION):
+        image.image(mode)
     image.icon()
-    index.html()
+
+
+def finish():
+    """Build the DJ database and html index."""
+    database.dj()
+    images()
+    html.index()
     print("Done.")
