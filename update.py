0811243e09bad1268b1136c880369d9a9a028078
Ching Chow <smwst10@gmail.com>
2012-10-17 17:23:00 -0400

    Support multiple threads per mode.

--- update.py
+++ update.py
@@ -9,32 +9,40 @@ import image
 import index
 
 
-def _update(name, mode, stop):
+def _update(mode, stop, lock):
     """Continuously update the specified database."""
+    # the cycle variable is used to prevent hammering the DJMAX site.  it sets
+    # the minimum hours to take to update all databases within an index by a
+    # single thread.  the actual time may exceed the specified cycle.  e.g. if
+    # cycle is set to zero.  when multiple threads are used, the actual time
+    # required to update all databases within an index is greater than or equal
+    # to the cycle divided by the number of threads.
     if mode == _.STAR:
-        hours = 36
+        cycle = 48
     elif mode == _.POP:
-        hours = 20
+        cycle = 24
     elif mode == _.CLUB:
-        hours = 12
+        cycle = 36
     elif mode == _.MISSION:
-        hours = 12
+        cycle = 36
     else:
         raise ValueError("invalid game mode")
     while not stop.is_set():
-        data = index.index(mode)
-        wait = (hours * 60 * 60 / len(data))
-        next = sorted(data.keys(), key=lambda x: data[x]["timestamp"], reverse=True).pop()
+        with lock:
+            data = index.index(mode)
+            next = sorted(data.keys(), key=lambda x: data[x]["timestamp"], reverse=True).pop()
+            index.touch_time(mode, next)
         database.build(mode, next)
-        stop.wait(wait)
-    print(name, "thread stopped.")
+        stop.wait(cycle * 60 * 60 / len(data))
 
 
 def update():
     """Continuously update the databases."""
     stop = threading.Event()
-    for mode_str, mode in (("Star", _.STAR), ("Pop", _.POP), ("Club", _.CLUB), ("Mission", _.MISSION)):
-        threading.Thread(target=_update, args=(mode_str, mode, stop)).start()
+    for mode in (_.STAR, _.POP, _.CLUB, _.MISSION):
+        lock = threading.Lock()
+        threading.Thread(target=_update, args=(mode, stop, lock)).start()
+        threading.Thread(target=_update, args=(mode, stop, lock)).start()
     try:
         while True:
             time.sleep(10000)
