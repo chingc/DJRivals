a0d30c4b7c51c720ef01d3073df1cd47b14854ae
Ching Chow <smwst10@gmail.com>
2012-10-24 17:20:27 -0400

    Inclusion of rival rankings table.

--- djrivals.js
+++ djrivals.js
@@ -5,15 +5,46 @@ $(document).ready(function () {
             rivals: []
         },
         ranking_table = function (data) {
-            var ranking = data.ranking,
+            var players = $.extend([], settings.rivals).concat(settings.me),
+                no_play = [],
                 dj_records = [],
                 rival_records = [],
+                exit_on_zero = 5,
+                found,
                 i,
                 ilen;
+            no_play = $.map(data, function (element) {
+                return element[2];
+            });
+            no_play = $.map(players, function (name, index) {
+                if ($.inArray(name, no_play) < 0) {
+                    players.splice(index, 1, false);
+                    return name;
+                }
+            });
+            players = $.map(players, function (name) {
+                if (name) {
+                    return name;
+                }
+            });
             dj_records.push("<table><tr><th>Rank</th><th>Icon</th><th>DJ</th><th>Score</th></tr>");
             rival_records.push("<table><tr><th>Rank</th><th>Icon</th><th>Rival DJ</th><th>Score</th></tr>");
-            for (i = 0, ilen = ranking.length; i < ilen; i += 1) {
-                dj_records.push("<tr><td>" + ranking[i][0] + '</td><td><img src="../images/icon/' + ranking[i][1] + '" /></td><td>' + ranking[i][2] + "</td><td>" + ranking[i][3] + "</td></tr>");
+            for (i = 0, ilen = data.length; i < ilen; i += 1) {
+                dj_records.push("<tr><td>" + data[i][0] + '</td><td><img src="../images/icon/' + data[i][1] + '" /></td><td>' + data[i][2] + "</td><td>" + data[i][3] + "</td></tr>");
+                found = $.inArray(data[i][2], players);
+                if (found > -1) {
+                    rival_records.push("<tr><td>" + data[i][0] + '</td><td><img src="../images/icon/' + data[i][1] + '" /></td><td>' + data[i][2] + "</td><td>" + data[i][3] + "</td></tr>");
+                    players.splice(found, 1);
+                }
+                if (players.length < 1) {
+                    if (exit_on_zero < 1) {
+                        break;
+                    }
+                    exit_on_zero -= 1;
+                }
+            }
+            for (i = 0, ilen = no_play.length; i < ilen; i += 1) {
+                rival_records.push("<tr><td>-</td><td></td><td>" + no_play[i] + "</td><td>-</td></tr>");
             }
             return '<table><tr><td class="djrank">' + dj_records.join("") + '</table></td><td class="rivalrank">' + rival_records.join("") + "</table></td></tr></table>";
         },
@@ -27,9 +58,9 @@ $(document).ready(function () {
                     dataType: "json",
                     url: "../database/" + (chart.length === 2 ? "pop_" + chart : chart).toLowerCase() + "/" + name + ".json"
                 }).done(function (data) {
-                    div.last().empty().html(ranking_table(data));
+                    div.last().empty().html(ranking_table(data.ranking));
                 }).fail(function () {
-                    alert("message on failure");
+                    div.last().empty().html("Unable to retrieve data.");
                 });
             }
         };
