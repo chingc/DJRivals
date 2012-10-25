58bf742719101eea880f14bb44b7a20eceb04c5e
Ching Chow <smwst10@gmail.com>
2012-10-24 20:15:30 -0400

    Exits on six instead of five when all DJ lists are empty.

--- djrivals.js
+++ djrivals.js
@@ -30,17 +30,17 @@ $(document).ready(function () {
             dj_records.push("<table><tr><th>Rank</th><th>Icon</th><th>DJ</th><th>Score</th></tr>");
             rival_records.push("<table><tr><th>Rank</th><th>Icon</th><th>Rival DJ</th><th>Score</th></tr>");
             for (i = 0, ilen = data.length; i < ilen; i += 1) {
-                dj_records.push("<tr><td>" + data[i][0] + '</td><td><img src="./images/icon/' + data[i][1] + '" /></td><td>' + data[i][2] + "</td><td>" + data[i][3] + "</td></tr>");
-                found = $.inArray(data[i][2], players);
-                if (found > -1) {
-                    rival_records.push("<tr><td>" + data[i][0] + '</td><td><img src="./images/icon/' + data[i][1] + '" /></td><td>' + data[i][2] + "</td><td>" + data[i][3] + "</td></tr>");
-                    players.splice(found, 1);
-                }
                 if (players.length < 1) {
                     if (exit_on_zero < 1) {
                         break;
                     }
                     exit_on_zero -= 1;
+                }
+                dj_records.push("<tr><td>" + data[i][0] + '</td><td><img src="./images/icon/' + data[i][1] + '" /></td><td>' + data[i][2] + "</td><td>" + data[i][3] + "</td></tr>");
+                found = $.inArray(data[i][2], players);
+                if (found > -1) {
+                    rival_records.push("<tr><td>" + data[i][0] + '</td><td><img src="./images/icon/' + data[i][1] + '" /></td><td>' + data[i][2] + "</td><td>" + data[i][3] + "</td></tr>");
+                    players.splice(found, 1);
                 }
             }
             for (i = 0, ilen = no_play.length; i < ilen; i += 1) {
