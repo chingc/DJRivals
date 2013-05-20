a783832d4716e10cfc87d509f4ac72682ae45af3
Ching Chow <ching.chow.sc@gmail.com>
2013-05-20 05:19:47 -0400

    Implemented master() to generate master scores.

--- database.py
+++ database.py
@@ -39,7 +39,7 @@ def create(mode, name):
 
 
 def dj():
-    """Create a record for each DJ in the local database."""
+    """Use the local database to create a record for each DJ."""
     def extract(directory):
         names = dict()
         for record in ls(directory):
@@ -56,7 +56,7 @@ def dj():
             for dj in disc["ranking"]:
                 all_dj[dj["djname"]][mode][disc["name"]] = (dj["rank"], dj["score"])
 
-    all_type    = ("star", "pop_nm", "pop_hd", "pop_mx", "pop_ex", "club", "mission")
+    all_type    = (game.mode.star, game.mode.pop_nm, game.mode.pop_hd, game.mode.pop_mx, game.mode.pop_ex, game.mode.club, game.mode.mission)
     all_db_path = (path.db.star, path.db.nm, path.db.hd, path.db.mx, path.db.ex, path.db.club, path.db.mission)
 
     # extract all djnames and disc/club/mission names
@@ -90,92 +90,26 @@ def dj():
 
 
 def master():
-    """Create the master score records."""
-    pass
+    """Use the local DJ database to create the master score records."""
+    pop_master = {}
 
+    for mode in (game.mode.star, game.mode.pop_nm, game.mode.pop_hd, game.mode.pop_mx, game.mode.pop_ex, game.mode.club, game.mode.mission):
+        master = []
+        for record in ls(path.db.dj):
+            with open(path.db.dj + record, "rb") as f:
+                record = json.loads(f.read().decode())
+            master_score = sum([v[1] for k, v in record[mode].items()])
+            if master_score:
+                master.append((record["icon"], record["name"], master_score))
+            if mode in (game.mode.pop_nm, game.mode.pop_hd, game.mode.pop_mx, game.mode.pop_ex):
+                if record["name"] not in pop_master:
+                    pop_master[record["name"]] = [record["icon"], record["name"], master_score]
+                else:
+                    pop_master[record["name"]][2] += master_score
+        master = [[rank + 1, dj[0], dj[1], dj[2]] for rank, dj in enumerate(sorted(master, key=lambda x: x[2], reverse=True))]
+        with open("{}{}.json".format(path.db.master, mode), "wb") as f:
+            f.write(json.dumps({"ranking": master}, indent=2).encode())
 
-#def dj():
-#    """Create a database of scores for each DJ using the local database."""
-#    def _extract(dj_set, directory):
-#        """Extract DJ and name data."""
-#        names = dict()
-#        for json_file in _list_dir(directory):
-#            with open(directory + json_file, "rb") as f:
-#                data = json.loads(f.read().decode())
-#            dj_set.update((record[1], record[2]) for record in data["ranking"])
-#            names[data["name"]] = [9999, 0]
-#        names["_master_"] = [9999, 0]
-#        return names
-#
-#    def _fill(dj_dict, directory, mode):
-#        """Fill scores for each DJ."""
-#        for json_file in _list_dir(directory):
-#            with open(directory + json_file, "rb") as f:
-#                data = json.loads(f.read().decode())
-#            for record in data["ranking"]:
-#                dj_dict[record[2]][mode][data["name"]] = [record[0], record[3]]
-#
-#    djs = set()
-#
-#    # extract disc, disc set, and mission names
-#    extracted = dict([
-#        ("star", _extract(djs, _.STAR_DB_DIR)),
-#        ("pop_nm", _extract(djs, _.POP_NM_DB_DIR)),
-#        ("pop_hd", _extract(djs, _.POP_HD_DB_DIR)),
-#        ("pop_mx", _extract(djs, _.POP_MX_DB_DIR)),
-#        ("pop_ex", _extract(djs, _.POP_EX_DB_DIR)),
-#        ("club", _extract(djs, _.CLUB_DB_DIR)),
-#        ("mission", _extract(djs, _.MISSION_DB_DIR))
-#    ])
-#
-#    # convert the DJ set into a dictionary
-#    djs = {dj[1]: dict([("name", dj[1]), ("icon", dj[0])]) for dj in djs}
-#
-#    # insert extracted into the DJ set
-#    for mode in extracted:
-#        for dj in djs:
-#            djs[dj][mode] = dict(extracted[mode])
-#
-#    # fill scores
-#    _fill(djs, _.STAR_DB_DIR, "star")
-#    _fill(djs, _.POP_NM_DB_DIR, "pop_nm")
-#    _fill(djs, _.POP_HD_DB_DIR, "pop_hd")
-#    _fill(djs, _.POP_MX_DB_DIR, "pop_mx")
-#    _fill(djs, _.POP_EX_DB_DIR, "pop_ex")
-#    _fill(djs, _.CLUB_DB_DIR, "club")
-#    _fill(djs, _.MISSION_DB_DIR, "mission")
-#
-#    # calculate, fill, and write master scores
-#    for mode in extracted:
-#        extracted[mode] = filter(lambda x: x[2] > 0, ((djs[dj]["icon"], dj, sum(score[1] for score in djs[dj][mode].values())) for dj in djs))
-#        extracted[mode] = [(rank + 1, info[0], info[1], info[2]) for rank, info in enumerate(sorted(extracted[mode], key=lambda x: x[2], reverse=True))]
-#        for info in extracted[mode]:
-#            djs[info[2]][mode]["_master_"] = [info[0], info[3]]
-#        with open(_.MASTER_DB_DIR + mode + ".json", "wb") as f:
-#            f.write(json.dumps({"ranking": extracted[mode]}, indent=None).encode())
-#
-#    # write pop master (overall) scores
-#    for mode in ["star", "club", "mission"]:
-#        del extracted[mode]
-#    extracted = sorted(([info[1], info[2], info[3]] for mode in extracted for info in extracted[mode]), key=lambda x: x[1])
-#    try:
-#        pop = [extracted.pop()]
-#    except IndexError:
-#        pass
-#    else:
-#        while len(extracted) > 0:
-#            next = extracted.pop()
-#            if next[1] == pop[-1][1]:
-#                pop[-1][2] += next[2]
-#            else:
-#                pop.append(next)
-#        pop = [(rank + 1, score[0], score[1], score[2]) for rank, score in enumerate(sorted(pop, key=lambda x: x[2], reverse=True))]
-#        with open(_.MASTER_DB_DIR + "pop.json", "wb") as f:
-#            f.write(json.dumps({"ranking": pop}, indent=None).encode())
-#
-#    # write DJ files and index
-#    for dj, scores in djs.items():
-#        with open("{}{}.json".format(_.DJ_DB_DIR, zlib.crc32(dj.encode())), "wb") as f:
-#            f.write(json.dumps(scores, indent=None).encode())
-#    with open(_.DJ_INDEX, "wb") as f:
-#        f.write(json.dumps([{"id": zlib.crc32(dj.encode()), "name": dj} for dj in sorted(djs.keys())], indent=None).encode())
+    pop_master = [[rank + 1, dj[0], dj[1], dj[2]] for rank, dj in enumerate(sorted(pop_master.values(), key=lambda x: x[2], reverse=True))]
+    with open("{}{}.json".format(path.db.master, "pop"), "wb") as f:
+        f.write(json.dumps({"ranking": pop_master}, indent=2).encode())
