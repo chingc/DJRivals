0fa56d7896cd07f316d6e4d7b8039445d3b2266c
Ching Chow <ching.chow.sc@gmail.com>
2013-05-20 17:26:23 -0400

    Cleaned and refactored.

--- image.py
+++ image.py
@@ -1,62 +1,41 @@
 """Image retrieval."""
 import json
 
-from common import _, _clean, _exists, _list_dir, _make_dir, _open_url
+from common import clean, exists, ls, urlopen_image, urlopen_json
+from settings import path, site, url
 
 
-def image(mode):
+def disc():
     """Download disc, disc set, and mission icons from the DJMAX site.
 
     An icon will be skipped if it is determined that it already exists.
     Existence is checked by a simple path check.
 
     """
-    if mode == _.STAR:
-        id_url  = _.STAR_ID_URL
-        img_url = _.DISC_IMAGE_URL
-        img_dir = _.DISC_IMAGE_DIR
-        keys    = _.DISC_KEY
-        last    = _.STAR_PAGES
-        suffix  = 2
-    elif mode == _.POP:
-        id_url  = _.POP_ID_URL
-        img_url = _.DISC_IMAGE_URL
-        img_dir = _.DISC_IMAGE_DIR
-        keys    = _.DISC_KEY
-        last    = _.POP_PAGES
-        suffix  = 5
-    elif mode == _.CLUB:
-        id_url  = _.CLUB_ID_URL
-        img_url = _.CLUB_IMAGE_URL
-        img_dir = _.CLUB_IMAGE_DIR
-        keys    = _.CLUB_KEY
-        last    = _.CLUB_PAGES
-        suffix  = 2
-    elif mode == _.MISSION:
-        id_url  = _.MISSION_ID_URL
-        img_url = _.MISSION_IMAGE_URL
-        img_dir = _.MISSION_IMAGE_DIR
-        keys    = _.MISSION_KEY
-        last    = _.MISSION_PAGES
-        suffix  = 2
-    else:
-        raise ValueError("invalid game mode")
-    for page in range(1, last + 1):
-        data = _open_url(id_url.format(page), "retrieving image name")
-        data = json.loads(data.read().decode())["DATA"]["RECORD"]
-        for record in data:
-            theirname = record[keys["image"]] + (".png" if mode == _.MISSION else "")
-            i = theirname.rfind(".")
-            name, extension = theirname[:i], theirname[i:]
-            for i in range(1, suffix):
-                myname = "{}_{}{}".format(_clean(record[keys["name"]]), i, extension)
-                if _exists(img_dir + myname):
+    all_id_urls  = (url.id.star, url.id.pop, url.id.club, url.id.mission)
+    all_img_urls = (url.img.star, url.img.pop, url.img.club, url.img.mission)
+    all_img_dirs = (path.img.star, path.img.pop, path.img.club, path.img.mission)
+    all_keys     = (site.key.star, site.key.pop, site.key.club, site.key.mission)
+    all_pages    = (site.pages.star, site.pages.pop, site.pages.club, site.pages.mission)
+    all_suffixes = (1, 4, 1, 1)  # image suffix ranges
+
+    for id_url, img_url, img_dir, key, end, suffix in zip(all_id_urls, all_img_urls, all_img_dirs, all_keys, all_pages, all_suffixes):
+        images = []
+        for page in range(1, end + 1):
+            for record in urlopen_json(id_url.format(page), "Retrieving image name"):
+                images.append((record[key["image"]] + (".png" if key == site.key.mission else ""), clean(record[key["name"]])))
+        for image in images:
+            unclean_name = image[0]
+            name, ext = image[0].rsplit(".", 1)
+            for i in range(1, suffix + 1):
+                clean_name = "{}_{}.{}".format(image[1], i, ext)
+                if exists(img_dir + clean_name):
                     continue
-                if mode == _.POP:
-                    theirname = "{}{}{}".format(name[:-1], i, extension)
-                with open(img_dir + myname, "wb") as f:
-                    f.write(_open_url(img_url.format(theirname), "downloading title image").read())
-                print('Wrote: "{}{}"'.format(img_dir, myname))
+                if key == site.key.pop:
+                    unclean_name = "{}{}.{}".format(name[:-1], i, ext)
+                with open(img_dir + clean_name, "wb") as f:
+                    f.write(urlopen_image(img_url.format(unclean_name)))
+                print('Wrote: "{}{}"'.format(img_dir, clean_name))
 
 
 def icon():
@@ -68,27 +47,14 @@ def icon():
 
     """
     icons = set()
-    for directory in [_.STAR_DB_DIR, _.POP_NM_DB_DIR, _.POP_HD_DB_DIR, _.POP_MX_DB_DIR, _.POP_EX_DB_DIR, _.CLUB_DB_DIR, _.MISSION_DB_DIR]:
-        for json_file in _list_dir(directory):
-            with open(directory + json_file, "rb") as f:
+    for db in (path.db.star, path.db.nm, path.db.hd, path.db.mx, path.db.ex, path.db.club, path.db.mission):
+        for record in ls(db):
+            with open(db + record, "rb") as f:
                 data = json.loads(f.read().decode())
-            icons.update([record[1] for record in data["ranking"]])
+            icons.update([dj["djicon"] for dj in data["ranking"]])
     for icon in icons:
-        if _exists(_.ICON_IMAGE_DIR + icon):
+        if exists(path.img.icon + icon):
             continue
-        with open(_.ICON_IMAGE_DIR + icon, "wb") as f:
-            f.write(_open_url(_.ICON_IMAGE_URL.format(icon), "downloading DJ icon").read())
-        print('Wrote: "{}{}"'.format(_.ICON_IMAGE_DIR, icon))
-
-
-_make_dir(_.DISC_IMAGE_DIR)
-_make_dir(_.CLUB_IMAGE_DIR)
-_make_dir(_.MISSION_IMAGE_DIR)
-_make_dir(_.ICON_IMAGE_DIR)
-_make_dir(_.STAR_DB_DIR)
-_make_dir(_.POP_NM_DB_DIR)
-_make_dir(_.POP_HD_DB_DIR)
-_make_dir(_.POP_MX_DB_DIR)
-_make_dir(_.POP_EX_DB_DIR)
-_make_dir(_.CLUB_DB_DIR)
-_make_dir(_.MISSION_DB_DIR)
+        with open(path.img.icon + icon, "wb") as f:
+            f.write(urlopen_image(url.img.icon.format(icon)))
+        print('Wrote: "{}{}"'.format(path.img.icon, icon))
