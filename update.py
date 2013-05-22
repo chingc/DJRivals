5af669d8f72c4766ac8abe46f45e2516d48f6e60
Ching Chow <ching.chow.sc@gmail.com>
2013-05-22 05:36:59 -0400

    Replaced _update() and update() with db().

--- update.py
+++ update.py
@@ -2,51 +2,59 @@
 import threading
 import time
 
-from common import _
+from settings import game
 import database
-import html
+#import html
 import image
 import index
 
 
-def _update(mode, stop, lock, thread_name):
-    """Continuously update the specified database."""
-    # the interval is used to prevent hammering the DJMAX site.  it sets the
-    # number of seconds to wait before downloading score data again.
-    if mode == _.STAR:
-        interval = 900
-    elif mode == _.POP:
-        interval = 600
-    elif mode == _.CLUB:
-        interval = 1200
-    elif mode == _.MISSION:
-        interval = 1800
-    else:
-        raise ValueError("invalid game mode")
-    while not stop.is_set():
-        with lock:
-            data = index.index(mode)
-            next = sorted(data.keys(), key=lambda x: data[x]["timestamp"])[0]
-            index.touch_time(mode, next)
-        database.build(mode, next)
-        stop.wait(interval)
-    print(thread_name, "has ended.")
-
-
-def update(threads=2):
+def db(threads=2):
     """Continuously update the databases."""
+    def oldest_last(mode):
+        data = index.read()
+        return sorted(data[mode].keys(), key=lambda x: data[mode][x]["timestamp"], reverse=True)
+
+    def thread(mode, stop, lock):
+        # the interval is used to prevent hammering the DJMAX site.  it sets the
+        # number of seconds to wait before downloading score data again.
+        if mode == game.mode.star: interval = 900
+        elif mode == game.mode.pop: interval = 600
+        elif mode == game.mode.club: interval = 1200
+        elif mode == game.mode.mission: interval = 1800
+        else: raise ValueError("Invalid game mode")
+        while not stop.is_set():
+            with lock:
+                next = names[mode].pop()
+            try:
+                database.create(mode, next)
+            except:
+                names[mode].append(next)
+                stop.set()
+                raise
+            else:
+                with lock:
+                    names[mode].insert(0, next)
+                    index.touch(mode, next)
+                stop.wait(interval)
+
+    names = {}
+    names[game.mode.star]    = oldest_last(game.mode.star)
+    names[game.mode.pop]     = oldest_last(game.mode.pop)
+    names[game.mode.club]    = oldest_last(game.mode.club)
+    names[game.mode.mission] = oldest_last(game.mode.mission)
+
     stops = []
-    for mode in (_.STAR, _.POP, _.CLUB, _.MISSION):
-        run = threads
+    for mode in (game.mode.star, game.mode.pop, game.mode.club, game.mode.mission):
         lock = threading.Lock()
-        while run > 0:
+        for t in range(threads):
             stop = threading.Event()
-            threading.Thread(target=_update, args=(mode, stop, lock, "{} thread {}".format(mode, run))).start()
+            threading.Thread(target=thread, args=(mode, stop, lock)).start()
             stops.append(stop)
-            run -= 1
     try:
         while True:
-            time.sleep(60)
+            print("{} of {} threads running.".format(threading.active_count() - 1, threads * 4), end="\r")
+            time.sleep(30)
     except KeyboardInterrupt:
         for stop in stops:
             stop.set()
@@ -56,11 +64,19 @@ def update(threads=2):
         print("Done.")
 
 
-def indexes():
-    """Update indexes."""
-    for mode in (_.STAR, _.POP, _.CLUB, _.MISSION):
-        index.index(mode, True)
-        image.image(mode)
+def other():
+    """Update the index and download any necessary images."""
+    start_time = time.monotonic()
+    index.create()
+    print("Index built (took: {:.2f}s)".format(time.monotonic() - start_time))
+
+    start_time = time.monotonic()
+    image.disc()
+    print("Disc icons downloaded (took: {:.2f}s)".format(time.monotonic() - start_time))
+
+    start_time = time.monotonic()
+    image.icon()
+    print("DJ icons downloaded (took: {:.2f}s)".format(time.monotonic() - start_time))
     print("Done.")
 
 
@@ -68,5 +84,5 @@ def sync():
     """Build the DJ database and front-end."""
     database.dj()
     image.icon()
-    html.pages()
+    #html.pages()
     print("Done.")
